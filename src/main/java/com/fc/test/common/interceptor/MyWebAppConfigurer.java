52883bdc1710320749605b1cb1f551aeccd80bea
WWX <15017814621@163.com>
2019-04-22 16:06:37 +0800

    备注信息

--- src/main/java/com/fc/test/common/interceptor/MyWebAppConfigurer.java
+++ src/main/java/com/fc/test/common/interceptor/MyWebAppConfigurer.java
@@ -19,56 +19,52 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  */
 @Configuration
 public class MyWebAppConfigurer  extends  WebMvcConfigurationSupport  {
-	
+
 	/** 解决跨域问题 **/
 	@Override
 	public void addCorsMappings(CorsRegistry registry){
 		//super.addCorsMappings(registry);
 	}
-	
+
 	/** 添加拦截器 **/
 	@Override
 	protected void addInterceptors(InterceptorRegistry registry){
 		registry.addInterceptor(new MyInterceptor());
 		super.addInterceptors(registry);
 	}
-	
+
 	/** 这里配置视图解析器 **/
 	@Override
 	protected void configureViewResolvers(ViewResolverRegistry registry){
 		super.configureViewResolvers(registry);
 	}
-	
+
 	/** 配置内容裁决的一些选项 **/
 	@Override
 	protected void configureContentNegotiation(ContentNegotiationConfigurer configurer){
 		super.configureContentNegotiation(configurer);
 	}
-	
+
 	/** 视图跳转控制器 **/
 	@Override
 	protected void addViewControllers(ViewControllerRegistry registry) {
-		
+
 		super.addViewControllers(registry);
 	}
-	
-	
+
+
 	/** 静态资源处理 **/
 	@Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		
+
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 
     }
 	/** 默认静态资源处理器 **/
-	
+
 	protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		//super.configureDefaultServletHandling(configurer);
 		//configurer.enable("stati");
 		configurer.enable();
 	}
-	
-	
-	
-	
 }
